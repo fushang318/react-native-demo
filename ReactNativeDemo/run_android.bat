1fdc25a1bb831590e927a10c572a5f52585fd16b
fushang318 <fushang318@gmail.com>
2016-08-12 17:17:38 +0800

    fix

--- ReactNativeDemo/run_android.bat
+++ ReactNativeDemo/run_android.bat
@@ -1,2 +1,2 @@
 cd android
-gradlew assembleDebug && adb install app\build\outputs\apk\app-debug.apk && cd .. && adb reverse tcp:8081 tcp:8081 && adb reverse tcp:8097 tcp:8097 && start react-native start
+gradlew assembleDebug && adb install app\build\outputs\apk\app-debug.apk && cd .. && adb reverse tcp:8081 tcp:8081 && start react-native start
